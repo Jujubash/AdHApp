8c9f5d7e6172537965e2d912db1061e5ce9b5ab8
Julia Vetter <julia.vetter@finlex.de>
2022-01-16 21:28:07 +0100

    Branch: GoogleGit Source

--- app/src/main/java/com/app/adhapp/MainActivity.java
+++ app/src/main/java/com/app/adhapp/MainActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private NsdManager nsdManager;
+    private NsdManager.RegistrationListener registrationListener;
+    private NsdManager.DiscoveryListener discoveryListener;
+    private Object nsdHelper;
+
     public static void main(String[] args) {
     }
 
@@ -13,5 +18,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        @Override
+        protected void onPause() {
+            if (nsdHelper != null) {
+                nsdHelper.tearDown();
+            }
+            super.onPause();
+        }
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+            if (nsdHelper != null) {
+                nsdHelper.registerService(connection.getLocalPort());
+                nsdHelper.discoverServices();
+            }
+        }
+
+        @Override
+        protected void onDestroy() {
+            nsdHelper.tearDown();
+            connection.tearDown();
+            super.onDestroy();
+        }
+
+        // NsdHelper's tearDown method
+        public void tearDown() {
+            nsdManager.unregisterService(registrationListener);
+            nsdManager.stopServiceDiscovery(discoveryListener);
+        }
     }
 }
---
+++ app/src/main/java/com/app/adhapp/connection.java
@@ -0,0 +1,12 @@
+package com.app.adhapp;
+
+public class connection {
+    private static int localPort;
+
+    public static int getLocalPort() {
+        return localPort;
+    }
+
+    public static void tearDown() {
+    }
+}
