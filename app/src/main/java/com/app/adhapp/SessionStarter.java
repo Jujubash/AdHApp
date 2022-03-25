db35a11ee4d717d0c504ceff27c4e4785a12b9eb
Julia Vetter <julia.vetter@finlex.de>
2022-03-25 09:41:17 +0100

    Changed: Session Activities

--- app/src/main/java/com/app/adhapp/SessionStarter.java
+++ app/src/main/java/com/app/adhapp/SessionStarter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.nsd.NsdManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,7 +18,7 @@ public class SessionStarter extends MainActivity {
     private String format_string;       // format_string is void offer(CharSequence[] formats)
     private static final String TAG = "Session Starter ";
     /** user interface related buttons and text input */
-    private Button button_start, button_search, button_search_format, button_start_format;
+    private Button button_start_format;
     private EditText edit_format;       // Search and Start Activity Inputs
     private TextView textview_format;   // Search and Start Activity Inputs
 
@@ -26,17 +27,31 @@ public class SessionStarter extends MainActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session_starter);
         Log.d(TAG, "onCreate() mode ");
-        /* creating session */
-        this.bluetoothSDPEngine = new BluetoothSDPEngine(this, null); // TODO: call constructor from BluetoothSDPEngine();
-        this.bluetoothSDPEngine.onCreate();
-        nsdManager = (NsdManager) this.getSystemService(Context.NSD_SERVICE);
-        this.discoverService = new BluetoothSDPEngine(this, null);
-        this.discoverService.onCreate();
-
+        /** users inputs format to search existing session */
+        button_start_format = (Button) findViewById(R.id.button_search_format);
+        button_start_format.setOnClickListener(v -> {
+            // create session and offer it:
+            if (format_string != null) {
+                sessionStarter(format_string);
+            }
+            else
+                Log.d(TAG, " Users input isn't valid");
+        });
         edit_format = (EditText)findViewById(R.id.edit_format);
         textview_format = (TextView)findViewById(R.id.textview_format);
         format_string = edit_format.getText().toString();
-        textview_format.setText("Session chosen: "+ format_string);
+        textview_format.setText("Session created: "+ format_string);
+    }
+
+    /**
+     * creating session
+     * @param format_string
+     */
+    protected void sessionStarter(String format_string) {
+        this.format_string = format_string;
+        this.bluetoothSDPEngine = new BluetoothSDPEngine(this, null); // TODO: call constructor from BluetoothSDPEngine();
+        this.bluetoothSDPEngine.onCreate();
+        nsdManager = (NsdManager) this.getSystemService(Context.NSD_SERVICE);
     }
 
     @Override
